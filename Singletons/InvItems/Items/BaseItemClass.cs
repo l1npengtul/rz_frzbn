3f38fd6685389b865cd3b6b8bb1179be4b8aefb1
l1npengtul <l1npengtul@protonmail.com>
2020-07-28 00:19:44 +0900

    start implement bast item class

--- Singletons/InvItems/Items/BaseItemClass.cs
+++ Singletons/InvItems/Items/BaseItemClass.cs
@@ -1,5 +1,43 @@
 using Godot;
+using ItemClass;
 
-public class BaseItemClass : Node{
-    private string itemName;
+namespace BaseItemClass{
+    public class BaseItemClass : Node2D{
+        [Export]
+        protected string itemModifierID;
+        [Export]
+        protected string itemNameID;
+        [Export]
+        protected string itemIcon;
+        [Export]
+        protected string itemInGameDisplay;
+        [Export]
+        protected string itemDescriptionID;
+        [Export]
+        protected ItemClass.ItemTypes itemTypes;
+
+        public string getItemModifier(){
+            return Tr(itemModifierID);
+        }
+        public void setItemModifier(string mod){
+            this.itemModifierID = mod;
+        }
+
+        public string getItemName(){
+            return Tr(itemNameID);
+        }
+        public string getFullItemName(){
+            return Tr(itemModifierID) + Tr(itemNameID);
+        }
+        public void setItemName(string mod){
+            this.itemNameID = mod;
+        }
+
+        public string getItemDesc(){
+            return Tr(itemDescriptionID);
+        }
+        public void setItemDesc(string mod){
+            this.itemDescriptionID = mod;
+        }
+    }
 }
---
+++ Singletons/InvItems/Items/Items.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+namespace ItemClass{
+
+    public enum ItemTypes{
+        Weapon,
+        FoodItem,
+        ConsumableItem,
+        OtherItem,
+    }
+
+    public class Items : Node {
+    }
+}
